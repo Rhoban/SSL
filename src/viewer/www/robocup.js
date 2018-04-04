1ba988e73f65b6b1f6a2331953d3b36b875196b4
Gregwar <g.passault@gmail.com>
2018-04-04 16:37:03 +0200

    Dragging ball, reseting view and lock/unlock the drag

--- src/viewer/www/robocup.js
+++ src/viewer/www/robocup.js
@@ -22,7 +22,8 @@ var robots = [
         y: 2,
         orientation: 0,
         color: 'blue',
-        present: true
+        present: true,
+        enabled: true
     },
     {
         id: 2,
@@ -30,7 +31,8 @@ var robots = [
         y: -2,
         orientation: 0,
         color: 'blue',
-        present: true
+        present: true,
+        enabled: true
     },
     {
         id: 2,
@@ -38,7 +40,8 @@ var robots = [
         y: 2,
         orientation: Math.PI,
         color: 'yellow',
-        present: false
+        present: false,
+        enabled: true
     },
     {
         id: 3,
@@ -46,7 +49,8 @@ var robots = [
         y: -1,
         orientation: Math.PI/1.5,
         color: 'yellow',
-        present: true
+        present: true,
+        enabled: true
     }
 ];
 
@@ -55,6 +59,23 @@ var ourColor = 'yellow';
 
 // Ball
 var ball = [0, 0];
+
+function robotById(id, color)
+{
+    if (typeof(color) == 'undefined') {
+        color = ourColor;
+    }
+
+    for (var k in robots) {
+        var robot = robots[k];
+
+        if (robot.id == id && robot.color == color) {
+            return robot;
+        }
+    }
+
+    return null;
+}
 
 class Viewer
 {
@@ -66,6 +87,10 @@ class Viewer
         this.mousePos = null;
         this.ratio = null;
         this.dragging = false;
+        this.draggingRobot = null;
+        this.draggingBall = false;
+        this.rotatingRobot = null;
+        this.dragLock = false;
         this.dragBegin = null;
         this.viewOffset = [0, 0];
         this.startOffset = [0, 0];
@@ -76,6 +101,7 @@ class Viewer
         this.ctx = this.container.getContext('2d');
 
         setInterval(() => this.update(), 20);
+
         this.container.addEventListener('mousemove', (evt) => this.mouseMove(evt));
         this.container.addEventListener('mousedown', (evt) => this.mouseDown(evt));
         this.container.addEventListener('mouseup', (evt) => this.mouseUp(evt));
@@ -102,6 +128,20 @@ class Viewer
         return [X, Y];
     }
 
+    // Mouse angle since begining of the drag
+    mouseAngle(robot)
+    {
+        if (this.mousePos) {
+            var pos = this.mousePosMeters();
+            var dx = pos[0] - robot.x;
+            var dy = pos[1] - robot.y;
+
+            return Math.atan2(dy, dx);
+        }
+
+        return 0;
+    }
+
     // Resetting the ratio to fit the canvas zone
     resetRatio()
     {
@@ -109,6 +149,7 @@ class Viewer
         var ratio1 = this.width/(field.length + 2*field.borders);
         if (ratio2 < ratio1) this.ratio = ratio2;
         else this.ratio = ratio1;
+        this.viewOffset = [0, 0];
     }
 
     grColor(color)
@@ -150,7 +191,7 @@ class Viewer
         ctx.fillStyle = '#aaa';
         ctx.font = '12pt sans';
         if (this.mousePos) {
-            var pos = this.mousePosMeters();
+            let pos = this.mousePosMeters();
 
             ctx.fillText("X: "+pos[0].toFixed(2)+"m, Y: "+pos[1].toFixed(2)+"m", 10, 20);
 
@@ -164,8 +205,10 @@ class Viewer
 
         // Translating to the center
         ctx.translate(this.width/2, this.height/2);
+
         // Rescaling using ratio
         ctx.scale(this.ratio, -this.ratio);
+
         // Translating to the view offset
         ctx.translate(this.viewOffset[0], this.viewOffset[1]);
 
@@ -181,6 +224,33 @@ class Viewer
 
         // Drawing robots
         this.drawRobots();
+
+        if (this.mousePos) {
+            let pos = this.mousePosMeters();
+
+            // Dragging a robot
+            if (this.draggingRobot) {
+                let robot = robotById(this.draggingRobot[0], this.draggingRobot[1]);
+                robot = JSON.parse(JSON.stringify(robot));
+                robot.ghost = true;
+                robot.x = pos[0];
+                robot.y = pos[1];
+                this.drawRobot(robot);
+            }
+
+            // Rotating a robot
+            if (this.rotatingRobot) {
+                let robot = robotById(this.rotatingRobot[0], this.rotatingRobot[1]);
+                robot = JSON.parse(JSON.stringify(robot));
+
+                var angle = this.mouseAngle(robot);
+                console.log(angle);
+
+                robot.ghost = true;
+                robot.orientation = angle;
+                this.drawRobot(robot);
+            }
+        }
 
         // Drawing the ball
         this.drawBall();
@@ -245,19 +315,29 @@ class Viewer
     drawRobots()
     {
         var ctx = this.ctx;
+        var over = this.overRobot();
 
         for (var k in robots) {
-            this.drawRobot(robots[k]);
+            var robot = robots[k];
+            this.drawRobot(robot, over == robot);
         }
     }
 
-    drawRobot(robot)
+    drawRobot(robot, extra)
     {
         var ctx = this.ctx;
         var front = 0.75;
 
+        if (!('ghost' in robot) && this.rotatingRobot && this.rotatingRobot[0] == robot.id &&
+            this.rotatingRobot[1] == robot.color) {
+            return;
+        }
+
         ctx.save();
         ctx.globalAlpha = robot.present ? 1 : 0.5;
+        if ('ghost' in robot) {
+            ctx.globalAlpha = 0.5;
+        }
 
         ctx.save();
         if (this.reversed)
@@ -277,6 +357,17 @@ class Viewer
         ctx.font = '0.14pt sans';
         ctx.scale(1, -1);
         ctx.fillText(''+robot.id, this.sign*robot.x-0.05, -robot.y+0.065);
+
+        if (typeof(extra) != 'undefined' && extra) {
+            ctx.globalAlpha = 1;
+            ctx.font = '0.09pt monospace';
+            ctx.fillStyle = '#aaa';
+            ctx.fillText(robot.color+' #'+robot.id, robot.x+0.15, -robot.y-0.1);
+            ctx.fillText('x:'+robot.x.toFixed(2)+'m', robot.x+0.15, -robot.y);
+            ctx.fillText('y:'+robot.y.toFixed(2)+'m', robot.x+0.15, -robot.y+0.1);
+            ctx.fillText('t:'+(180*robot.orientation/Math.PI).toFixed(1)+'°', robot.x+0.15, -robot.y+0.2);
+        }
+
         ctx.restore();
         ctx.restore();
     }
@@ -285,21 +376,82 @@ class Viewer
     {
         var ctx = this.ctx;
 
+        let pos = JSON.parse(JSON.stringify(ball));
+        if (this.draggingBall && this.mousePos) {
+            pos = this.mousePosMeters();
+        }
+
         ctx.beginPath();
         ctx.fillStyle = 'orange';
-        ctx.arc(ball[0], ball[1], 0.043, 0, Math.PI*2);
+        ctx.arc(pos[0], pos[1], 0.043, 0, Math.PI*2);
         ctx.fill();
+
+        if (this.overBall()) {
+            ctx.save();
+            ctx.scale(1, -1);
+            ctx.font = '0.09pt monospace';
+            ctx.fillText('ball', pos[0]+0.15, -pos[1]-0.1);
+            ctx.fillText('x:'+pos[0].toFixed(2)+'m', pos[0]+0.15, -pos[1]);
+            ctx.fillText('y:'+pos[1].toFixed(2)+'m', pos[0]+0.15, -pos[1]+0.1);
+            ctx.restore();
+        }
+    }
+
+    overBall()
+    {
+        if (this.mousePos) {
+            var xy = this.mousePosMeters();
+            var dist = Math.sqrt(Math.pow(ball[0]-xy[0], 2) + Math.pow(ball[1]-xy[1], 2));
+
+            return dist < 0.1;
+        }
+
+        return false;
+    }
+
+    overRobot()
+    {
+        if (this.mousePos) {
+            var xy = this.mousePosMeters();
+            for (var k in robots) {
+                var robot = robots[k];
+                var dist = Math.sqrt(Math.pow(robot.x-xy[0], 2) + Math.pow(robot.y-xy[1], 2));
+                if (dist < 0.1) {
+                    return robot;
+                }
+            }
+        }
+
+        return null;
     }
 
     // Mouse is down
     mouseDown(evt)
     {
         if (this.mousePos) {
+            this.dragBegin = this.mousePos;
+
+            if (evt.buttons == 1) {
+                if (this.overBall()) {
+                    this.draggingBall = true;
+                } else {
+                    let over = this.overRobot();
+                    if (over) {
+                        this.draggingRobot = [over.id, over.color];
+                    } else if (!this.dragLock) {
+                        // Mouse3 to drag the view
+                        this.dragging = true;
+                        this.startOffset = this.viewOffset;
+                    }
+                }
+            }
+
             if (evt.buttons == 4) {
-                // Mouse3 to drag the view
-                this.dragging = true;
-                this.dragBegin = this.mousePos;
-                this.startOffset = this.viewOffset;
+                let over = this.overRobot();
+
+                if (over) {
+                    this.rotatingRobot = [over.id, over.color];
+                }
             }
         }
     }
@@ -307,7 +459,38 @@ class Viewer
     // Stopping the dragging
     mouseUp(evt)
     {
+        if (this.draggingRobot && this.mousePos) {
+            let robot = robotById(this.draggingRobot[0], this.draggingRobot[1]);
+            if (robot) {
+                var pos = this.mousePosMeters();
+
+                // XXX: Communicate with the API
+                robot.x = pos[0];
+                robot.y = pos[1];
+            }
+        }
+
+        if (this.rotatingRobot && this.mousePos) {
+            let robot = robotById(this.rotatingRobot[0], this.rotatingRobot[1]);
+            if (robot) {
+                var angle = this.mouseAngle(robot);
+
+                // XXX: Communicate with the API
+                robot.orientation = angle;
+            }
+        }
+
+        if (this.draggingBall && this.mousePos) {
+            let pos = this.mousePosMeters();
+
+            // XXX: Communicate with the API
+            ball = pos;
+        }
+
         this.dragging = false;
+        this.draggingRobot = null;
+        this.rotatingRobot = null;
+        this.draggingBall = false;
     }
 
     // Spinning the mouse wheel
@@ -325,7 +508,6 @@ class Viewer
     {
         if (evt.key == 'Escape' || evt.keyCode == 27) {
             this.resetRatio();
-            this.viewOffset = [0, 0];
             console.log(evt);
         }
     }
@@ -334,6 +516,29 @@ class Viewer
 class Manager
 {
     constructor() {
+        $('.panel-content.not-shown').hide();
+
+        $('.panel-block').each(function() {
+            let content = $(this).find('.panel-content');
+            var title = $(this).find('.panel-title');
+
+            if (content.is(':visible')) {
+                title.find('h4').prepend('<img class="expand-collapse" src="collapse.png"/>');
+            } else {
+                title.find('h4').prepend('<img class="expand-collapse" src="expand.png"/>');
+            }
+
+            title.click(function() {
+                if (content.is(':visible')) {
+                    content.hide();
+                    title.find('h4 .expand-collapse').attr('src', 'expand.png');
+                } else {
+                    content.show();
+                    title.find('h4 .expand-collapse').attr('src', 'collapse.png');
+                }
+            });
+        });
+
         var template = $('.robots').html();
         var html = '';
 
@@ -362,34 +567,24 @@ class Manager
                 $('.robot-'+id+' .icon-expand').hide();
             }
         });
-    }
-}
 
-class Panels
-{
-    constructor()
-    {
-        $('.panel-content.not-shown').hide();
+        $('.robots .enable-disable').click(function() {
+            var id = parseInt($(this).attr('rel'));
+            var robot = robotById(id);
 
-        $('.panel-block').each(function() {
-            let content = $(this).find('.panel-content');
-            var title = $(this).find('.panel-title');
+            if (robot) {
+                robot.enabled = !robot.enabled;
 
-            if (content.is(':visible')) {
-                title.find('h4').prepend('<img class="expand-collapse" src="collapse.png"/>');
-            } else {
-                title.find('h4').prepend('<img class="expand-collapse" src="expand.png"/>');
-            }
-
-            title.click(function() {
-                if (content.is(':visible')) {
-                    content.hide();
-                    title.find('h4 .expand-collapse').attr('src', 'expand.png');
+                if (robot.enabled) {
+                    $(this).removeClass('btn-success');
+                    $(this).addClass('btn-danger');
+                    $(this).text('Disable');
                 } else {
-                    content.show();
-                    title.find('h4 .expand-collapse').attr('src', 'collapse.png');
+                    $(this).addClass('btn-success');
+                    $(this).removeClass('btn-danger');
+                    $(this).text('Enable');
                 }
-            });
+            }
         });
     }
 }
@@ -398,11 +593,8 @@ $(document).ready(function() {
     // Instantiating the viewer
     var viewer = new Viewer();
 
-    // Robots manager
-    var manager = new Manager();
-
     // Panels manager
-    var panels = new Panels();
+    var manager = new Manager();
 
     $('.reverse-view').change(function() {
         viewer.reversed = $(this).is(':checked');
@@ -417,5 +609,21 @@ $(document).ready(function() {
             ourColor = 'yellow';
         }
         $(this).text('We are: '+ourColor);
+    });
+    $('.reset-view').click(function() {
+        viewer.resetRatio();
+    });
+    $('.drag-lock').click(function() {
+        viewer.dragLock = !viewer.dragLock;
+
+        if (viewer.dragLock) {
+            $(this).text('Unlock the drag');
+            $(this).addClass('btn-success');
+            $(this).removeClass('btn-danger');
+        } else {
+            $(this).text('Lock the drag');
+            $(this).addClass('btn-danger');
+            $(this).removeClass('btn-success');
+        }
     });
 });
