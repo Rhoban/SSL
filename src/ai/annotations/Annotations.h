a9165f20866987d62edfa3185d022f3d9323d146
Adrien Boussicault <boussica@labri.fr>
2018-06-11 09:55:03 +0200

    Add LGPL Licence in all files

--- src/ai/annotations/Annotations.h
+++ src/ai/annotations/Annotations.h
@@ -1,4 +1,24 @@
-#pragma once
+/*
+    This file is part of SSL.
+
+    Copyright 2018 Boussicault Adrien (adrien.boussicault@u-bordeaux.fr)
+
+    SSL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SSL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with SSL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __ANNOTATIONS__ANNOTATIONS_H__
+#define __ANNOTATIONS__ANNOTATIONS_H__ 
 
 #include <json/json.h>
 #include <vector>
@@ -61,3 +81,5 @@ namespace RhobanSSLAnnotation
         Json::Value json;
     };
 }
+
+#endif
