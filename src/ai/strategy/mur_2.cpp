d6688891f53b5f6e2af58eccae2b1d2de3012940
jbezamat <jeremy.bezamat@gmail.com>
2018-06-16 21:56:29 +0200

    Plan veschambre ok

--- src/ai/strategy/mur_2.cpp
+++ src/ai/strategy/mur_2.cpp
@@ -30,14 +30,14 @@ Mur_2::Mur_2(Ai::AiData & ai_data):
     Strategy(ai_data)
 {
 }
-    
+
 Mur_2::~Mur_2(){
-}        
+}
 
 /*
  * We define the minimal number of robot in the field.
  * The goalkeeper is not counted.
- */ 
+ */
 int Mur_2::min_robots() const {
     return 2;
 }
@@ -45,7 +45,7 @@ int Mur_2::min_robots() const {
 /*
  * We define the maximal number of robot in the field.
  * The goalkeeper is not counted.
- */ 
+ */
 int Mur_2::max_robots() const {
     return 2;
 }
@@ -65,6 +65,19 @@ void Mur_2::stop(double time){
 }
 
 void Mur_2::update(double time){
+
+  int nearest_ally_robot_from_ball = GameInformations::get_nearest_ball( Vision::Team::Ally );
+  is_closest_0 = false;
+  is_closest_1 = false;
+
+  if ( nearest_ally_robot_from_ball == player_id(0) ){
+      is_closest_0 = true;
+  } else {
+      if (nearest_ally_robot_from_ball == player_id(1)) {
+        is_closest_1 = true;
+      }
+  }
+
 }
 
 void Mur_2::assign_behavior_to_robots(
@@ -91,13 +104,15 @@ void Mur_2::assign_behavior_to_robots(
         assign_behavior( player_id(1), mur2 );
 
         behaviors_are_assigned = true;
+    } else {
+
     }
 }
 
 // We declare here the starting positions that are used to :
 //   - place the robot during STOP referee state
 //   - to compute the robot order of get_player_ids(),
-//     we minimize the distance between 
+//     we minimize the distance between
 //     the startings points and all the robot position, just
 //     before the start() or during the STOP referee state.
 std::list<
@@ -106,30 +121,30 @@ std::list<
     assert( min_robots() <= number_of_avalaible_robots );
     assert(
         max_robots()==-1 or
-        number_of_avalaible_robots <= max_robots() 
+        number_of_avalaible_robots <= max_robots()
     );
 
     return {
         std::pair<rhoban_geometry::Point,ContinuousAngle>(
-            ai_data.relative2absolute(-1.0/3.0, 0.0),
+            ally_goal_center(),
             0.0
         )
     };
 }
 
 //
-// This function return false if you don't want to 
-// give a staring position. So the manager will chose 
+// This function return false if you don't want to
+// give a staring position. So the manager will chose
 // a default position for you.
-// 
+//
 bool Mur_2::get_starting_position_for_goalie(
-    rhoban_geometry::Point & linear_position, 
+    rhoban_geometry::Point & linear_position,
     ContinuousAngle & angular_position
 ){
     linear_position =  ally_goal_center();
     angular_position = ContinuousAngle(0.0);
     return true;
-} 
+}
 
 
 
