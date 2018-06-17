0e228bd0e90c59d0014069002ddf8c9b5222afe8
Victor Mignot <victor.mgnt@gmail.com>
2018-06-17 10:07:12 -0400

    adding indirect and direct referee support

--- src/ai/referee/Referee.cpp
+++ src/ai/referee/Referee.cpp
@@ -28,6 +28,8 @@ const std::string Referee_Id::STATE_HALTED = "halted";
 const std::string Referee_Id::STATE_STOPPED = "stopped";
 const std::string Referee_Id::STATE_PREPARE_KICKOFF = "prepare_kickoff";
 const std::string Referee_Id::STATE_PREPARE_PENALTY = "prepare_penalty";
+const std::string Referee_Id::STATE_PREPARE_DIRECT = "prepare_direct";
+const std::string Referee_Id::STATE_PREPARE_INDIRECT = "prepare_indirect";
 const std::string Referee_Id::STATE_RUNNING = "running";
 const std::string Referee_Id::STATE_TIMEOUT = "timeout";
 
@@ -47,12 +49,18 @@ const std::string Referee_Id::EDGE_TIMEOUT_TO_HALTED = "halt_game_t";
 
 const std::string Referee_Id::EDGE_TIMEOUT_START = "timeout_start";
 const std::string Referee_Id::EDGE_FORCE_START = "force_start";
+
 const std::string Referee_Id::EDGE_KICKOFF_YELLOW = "kickoff_yellow";
 const std::string Referee_Id::EDGE_KICKOFF_BLUE = "kickoff_blue";
+
 const std::string Referee_Id::EDGE_PENALTY_BLUE = "penalty_blue";
 const std::string Referee_Id::EDGE_PENALTY_YELLOW = "penalty_yellow";
-const std::string Referee_Id::EDGE_INDIRECT = "indirect";
-const std::string Referee_Id::EDGE_DIRECT = "direct";
+
+const std::string Referee_Id::EDGE_INDIRECT_BLUE = "indirect_blue";
+const std::string Referee_Id::EDGE_INDIRECT_YELLOW = "indirect_yellow";
+
+const std::string Referee_Id::EDGE_DIRECT_BLUE = "direct_blue";
+const std::string Referee_Id::EDGE_DIRECT_YELLOW = "direct_yellow";
 
 const std::string Referee_Id::EDGE_NORMAL_START_FOR_KICKOFF = "normal_start_k";
 const std::string Referee_Id::EDGE_NORMAL_START_FOR_PENALTY = "normal_start_p";
@@ -231,6 +239,7 @@ Referee::Referee():
             team_having_kickoff = Ai::Blue;
         }
     );
+
     machine_state.add_edge(
         Referee_Id::EDGE_PENALTY_BLUE,
         Referee_Id::STATE_STOPPED, Referee_Id::STATE_PREPARE_PENALTY,
@@ -253,19 +262,53 @@ Referee::Referee():
             team_having_penalty = Ai::Yellow;
         }
     );
+
     machine_state.add_edge(
-        Referee_Id::EDGE_INDIRECT,
-        Referee_Id::STATE_STOPPED, Referee_Id::STATE_RUNNING,
-        command_is_one_of_<
-            SSL_Referee::INDIRECT_FREE_BLUE, SSL_Referee::INDIRECT_FREE_YELLOW
-        >
+        Referee_Id::EDGE_DIRECT_BLUE,
+        Referee_Id::STATE_STOPPED, Referee_Id::STATE_PREPARE_DIRECT,
+        command_is_<SSL_Referee::DIRECT_FREE_BLUE>,
+        [&](
+            const Referee_data & referee_data,
+            unsigned int run_number, unsigned int atomic_run_number
+        ){
+            team_having_direct = Ai::Blue;
+        }
     );
+
     machine_state.add_edge(
-        Referee_Id::EDGE_DIRECT,
-        Referee_Id::STATE_STOPPED, Referee_Id::STATE_RUNNING,
-        command_is_one_of_<
-            SSL_Referee::DIRECT_FREE_BLUE, SSL_Referee::DIRECT_FREE_YELLOW
-        >
+        Referee_Id::EDGE_DIRECT_YELLOW,
+        Referee_Id::STATE_STOPPED, Referee_Id::STATE_PREPARE_DIRECT,
+        command_is_<SSL_Referee::DIRECT_FREE_YELLOW>,
+        [&](
+            const Referee_data & referee_data,
+            unsigned int run_number, unsigned int atomic_run_number
+        ){
+            team_having_direct = Ai::Yellow;
+        }
+    );
+
+    machine_state.add_edge(
+        Referee_Id::EDGE_INDIRECT_BLUE,
+        Referee_Id::STATE_STOPPED, Referee_Id::STATE_PREPARE_INDIRECT,
+        command_is_<SSL_Referee::INDIRECT_FREE_BLUE>,
+        [&](
+            const Referee_data & referee_data,
+            unsigned int run_number, unsigned int atomic_run_number
+        ){
+            team_having_indirect = Ai::Blue;
+        }
+    );
+
+    machine_state.add_edge(
+        Referee_Id::EDGE_INDIRECT_YELLOW,
+        Referee_Id::STATE_STOPPED, Referee_Id::STATE_PREPARE_INDIRECT,
+        command_is_<SSL_Referee::INDIRECT_FREE_YELLOW>,
+        [&](
+            const Referee_data & referee_data,
+            unsigned int run_number, unsigned int atomic_run_number
+        ){
+            team_having_indirect = Ai::Yellow;
+        }
     );
 
     machine_state.add_edge(
@@ -357,6 +400,14 @@ Ai::Team Referee::kickoff_team() const {
 
 Ai::Team Referee::penalty_team() const {
     return team_having_penalty;
+}
+
+Ai::Team Referee::direct_team() const {
+    return team_having_direct;
+}
+
+Ai::Team Referee::indirect_team() const {
+    return team_having_indirect;
 }
 
 bool Referee::blue_have_it_s_goal_on_positive_x_axis() const {
--- src/ai/referee/Referee.h
+++ src/ai/referee/Referee.h
@@ -35,6 +35,8 @@ struct Referee_Id {
     static const std::string STATE_STOPPED;
     static const std::string STATE_PREPARE_KICKOFF;
     static const std::string STATE_PREPARE_PENALTY;
+    static const std::string STATE_PREPARE_DIRECT;
+    static const std::string STATE_PREPARE_INDIRECT;
     static const std::string STATE_RUNNING;
     static const std::string STATE_TIMEOUT;
 
@@ -58,8 +60,10 @@ struct Referee_Id {
     static const std::string EDGE_KICKOFF_BLUE;
     static const std::string EDGE_PENALTY_YELLOW;
     static const std::string EDGE_PENALTY_BLUE;
-    static const std::string EDGE_INDIRECT;
-    static const std::string EDGE_DIRECT;
+    static const std::string EDGE_INDIRECT_BLUE;
+    static const std::string EDGE_INDIRECT_YELLOW;
+    static const std::string EDGE_DIRECT_BLUE;
+    static const std::string EDGE_DIRECT_YELLOW;
 
     static const std::string EDGE_NORMAL_START_FOR_KICKOFF;
     static const std::string EDGE_NORMAL_START_FOR_PENALTY;
@@ -106,6 +110,8 @@ private:
 
     Ai::Team team_having_kickoff;
     Ai::Team team_having_penalty;
+    Ai::Team team_having_direct;
+    Ai::Team team_having_indirect;
 public:
     Referee();
 
@@ -116,6 +122,8 @@ public:
 
     Ai::Team kickoff_team() const ;
     Ai::Team penalty_team() const ;
+    Ai::Team direct_team() const ;
+    Ai::Team indirect_team() const ;
 
     bool blue_have_it_s_goal_on_positive_x_axis() const;
     Ai::Team get_team_color( const std::string & team_name ) const;
