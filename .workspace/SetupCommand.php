5e7ed617d4e95f22a46681bb678d451e63344e4e
medrimonia <medrimonia@gmail.com>
2018-07-05 22:44:54 +0200

    Removing avx flag from setup command

--- .workspace/SetupCommand.php
+++ .workspace/SetupCommand.php
@@ -27,8 +27,8 @@ class SetupCommand extends Command
         Terminal::info("* Runnin catkin init\n");
         OS::run('catkin init --workspace .');
         Terminal::info("* Setup of profile debug and release");
-        OS::run('catkin config --profile debug -x _debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -msse2 -mavx"');
-        OS::run('catkin config --profile profile -x _profile --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -pg -msse2 -mavx"');
-        OS::run('catkin config --profile release -x _release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -msse2 -mavx"');
+        OS::run('catkin config --profile debug -x _debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -msse2"');
+        OS::run('catkin config --profile profile -x _profile --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -pg -msse2"');
+        OS::run('catkin config --profile release -x _release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -msse2"');
     }
 }
