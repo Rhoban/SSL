b4b2b3225cda683fea8745d40466b89848a8b22f
Etienne Schmitz <hello@etienne-schmitz.com>
2019-02-01 17:53:05 +0100

    Create Begginer Tutorial - Defensor

--- src/ai/manager/Manual.cpp
+++ src/ai/manager/Manual.cpp
@@ -43,6 +43,7 @@
 #include <robot_behavior/pass.h>
 #include <robot_behavior/tutorials/begginer/begginer_go_corner.h>
 #include <robot_behavior/tutorials/begginer/goalie.h>
+#include <robot_behavior/tutorials/begginer/defensor.h>
 #include <robot_behavior/tutorials/intermediate/striker.h>
 #include <robot_behavior/tutorials/intermediate/prepare_strike.h>
 
@@ -441,6 +442,17 @@ Manual::Manual( Ai::AiData & ai_data ):
                 [&](double time, double dt){
                     Robot_behavior::Begginer_goalie* goalie = new Robot_behavior::Begginer_goalie(ai_data);
                     return std::shared_ptr<Robot_behavior::RobotBehavior>(goalie);
+                }, false // we don't want to define a goal here !
+            )
+        )
+    );
+    register_strategy(
+        "Begginer_Defensor", std::shared_ptr<Strategy::Strategy>(
+            new Strategy::From_robot_behavior(
+                ai_data,
+                [&](double time, double dt){
+                    Robot_behavior::Begginer_defensor* striker = new Robot_behavior::Begginer_defensor(ai_data);
+                    return std::shared_ptr<Robot_behavior::RobotBehavior>(striker);
                 }, false // we don't want to define a goal here !
             )
         )
