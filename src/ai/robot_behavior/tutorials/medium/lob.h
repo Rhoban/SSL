cbb723071c62714a75e1e4f82309c17e12025e49
Etienne Schmitz <hello@etienne-schmitz.com>
2019-02-08 19:30:39 +0100

    Begin Lob (Structure)

---
+++ src/ai/robot_behavior/tutorials/medium/lob.h
@@ -0,0 +1,54 @@
+/*
+    This file is part of SSL.
+
+    Copyright 2019 Schmitz Etienne (hello@etienne-schmitz.com)
+
+    SSL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SSL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with SSL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __ROBOT_BEHAVIOR__TUTORIALS__MEDIUM__LOB__
+#define __ROBOT_BEHAVIOR__TUTORIALS__MEDIUM__LOB__
+
+#include <robot_behavior/robot_behavior.h>
+#include <robot_behavior/factory.h>
+
+namespace RhobanSSL {
+namespace Robot_behavior {
+namespace Medium {
+/** Tutorial class to show how to move a robot in the side corner. */
+class Lob : public RobotBehavior  {
+    private:
+	ConsignFollower* follower;
+    RhobanSSLAnnotation::Annotations annotations;
+
+    public:
+    Lob(Ai::AiData& ai_data);
+
+    virtual void update(
+        double time,
+        const Ai::Robot & robot,
+        const Ai::Ball & ball
+    );
+
+	virtual Control control() const;
+
+    virtual RhobanSSLAnnotation::Annotations get_annotations() const;
+
+	virtual ~Lob();
+};
+};
+};
+}; //Namespace Rhoban
+
+#endif
