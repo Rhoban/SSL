a7cca9c3a9266c171ec39a81df49f5b16c794f87
Etienne Schmitz <hello@etienne-schmitz.com>
2019-02-08 13:57:14 +0100

    Correct the bug about the define & Add a new behavior robot_near_ball.

--- src/ai/manager/Manual.cpp
+++ src/ai/manager/Manual.cpp
@@ -45,6 +45,7 @@
 #include <robot_behavior/tutorials/begginer/goalie.h>
 #include <robot_behavior/tutorials/begginer/defensor.h>
 #include <robot_behavior/tutorials/begginer/see_ball.h>
+#include <robot_behavior/tutorials/begginer/robot_near_ball.h>
 #include <robot_behavior/tutorials/begginer/annotations_ball_position.h>
 #include <robot_behavior/tutorials/intermediate/striker.h>
 #include <robot_behavior/tutorials/intermediate/prepare_strike.h>
@@ -478,6 +479,17 @@ Manual::Manual( Ai::AiData & ai_data ):
                 [&](double time, double dt){
                     Robot_behavior::Begginer_see_ball* see_ball = new Robot_behavior::Begginer_see_ball(ai_data);
                     return std::shared_ptr<Robot_behavior::RobotBehavior>(see_ball);
+                }, false // we don't want to define a goal here !
+            )
+        )
+    );
+    register_strategy(
+        "Begginer - Robot near ball", std::shared_ptr<Strategy::Strategy>(
+            new Strategy::From_robot_behavior(
+                ai_data,
+                [&](double time, double dt){
+                    Robot_behavior::Begginer_robot_near_ball* near_ball = new Robot_behavior::Begginer_robot_near_ball(ai_data);
+                    return std::shared_ptr<Robot_behavior::RobotBehavior>(near_ball);
                 }, false // we don't want to define a goal here !
             )
         )
