e8acff3e286c20086985c50c082c69633526e663
Etienne Schmitz <hello@etienne-schmitz.com>
2019-05-08 10:50:41 +0200

    Add the libwebsocket to communicate with the viewer

--- src/ai/viewer.cpp
+++ src/ai/viewer.cpp
@@ -1,36 +1,81 @@
 #include "viewer.h"
 #include "debug.h"
 
-rhoban_ssl::ViewerCommunication::ViewerCommunication()
-  : context_(1), from_viewer_(context_, ZMQ_PULL), to_viewer_(context_, ZMQ_PUSH)
-{
-  from_viewer_.connect("tcp://127.0.0.1:5555");
-  to_viewer_.bind("tcp://127.0.0.1:5556");
-  zmq_poll_ = new zmq::pollitem_t[2];
-  zmq_poll_[0].socket = static_cast<void*>(from_viewer_);
-  zmq_poll_[0].events = ZMQ_POLLIN;
-  zmq_poll_[1].socket = static_cast<void*>(to_viewer_);
-  zmq_poll_[1].events = ZMQ_POLLOUT;
+int rhoban_ssl::ViewerCommunication::callback_http_dummy(struct lws *wsi, enum lws_callback_reasons reason,
+                        void *user, void *in, size_t len) {return 0;}
+
+int rhoban_ssl::ViewerCommunication::callback_viewer(struct lws *wsi, enum lws_callback_reasons reason,
+                        void *user, void *in, size_t len) {
+    unsigned char m_Test[20] = "Hello World";
+    switch (reason)
+    {
+    case LWS_CALLBACK_ESTABLISHED:
+            std::cout << "we receive a client" << std::endl;
+            return 0;
+            break;
+    case LWS_CALLBACK_RECEIVE:
+        std::cout << "we receive something" << std::endl;
+        std::cout << len << std::endl;
+        std::cout << (char *) in << std::endl;
+        return 0;
+        break;
+    case LWS_CALLBACK_SERVER_WRITEABLE:
+        lws_write(wsi, m_Test, 20, LWS_WRITE_TEXT);
+        return 0;
+        break;
+    //case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+      //  std::cout << "we receive client refused";
+        //return 0;
+        //break;
+    default:
+        return 0;
+        break;
+    }
+
 }
+
+rhoban_ssl::ViewerCommunication::ViewerCommunication()
+{
+      // Set the protocols
+
+    protocols_[0] = { "http", rhoban_ssl::ViewerCommunication::callback_http_dummy, 0, 0 };
+    protocols_[1] =   {
+            "viewer_protocol",
+            rhoban_ssl::ViewerCommunication::callback_viewer,
+            sizeof(per_session_data_minimal),
+            1024,
+            0,
+            NULL
+    };
+    protocols_[2] = {
+            NULL, NULL, 0,0
+    };
+
+      struct lws_context_creation_info info;
+      memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+
+      info.port = 7880;
+      info.protocols = protocols_;
+
+      context_ = lws_create_context(&info);
+}
+
+
 
 bool rhoban_ssl::ViewerCommunication::runTask()
 {
   try
   {
-    zmq::poll(zmq_poll_, 2, 0);
-    zmq::message_t message;
-    if (zmq_poll_[0].revents & ZMQ_POLLIN)
-    {
-      from_viewer_.recv(&message);
-      DEBUG(message.data<char>());
-    }
-    if (zmq_poll_[1].revents & ZMQ_POLLOUT)
-    {
-      to_viewer_.send("world\n", 6, 0);
-    }
+     // Use this to ask to send.
+     //lws_callback_on_writable_all_protocol(context_, &protocols[1]);
+     lws_service(context_, 10);
   }
   catch (...)
   {
   }
   return true;
+}
+
+rhoban_ssl::ViewerCommunication::~ViewerCommunication() {
+    lws_context_destroy(context_);
 }
--- src/ai/viewer.h
+++ src/ai/viewer.h
@@ -1,19 +1,86 @@
 #pragma once
 
 #include <execution_manager.h>
-#include <zmq.hpp>
+#include <libwebsockets.h>
+#include <iostream>
+
+/**
+ * @brief The per_session_data__minimal struct
+ *
+ * One of these is created for each client connecting to us
+ */
+struct per_session_data_minimal {
+        struct per_session_data_minimal *pss_list;
+        struct lws *wsi;
+        int last;
+};
 
 namespace rhoban_ssl
 {
+/**
+ * @brief The class to communicate with the viewer.
+ */
 class ViewerCommunication : public Task
 {
-  zmq::context_t context_;
-  zmq::socket_t from_viewer_;
-  zmq::socket_t to_viewer_;
-  zmq::pollitem_t* zmq_poll_;
+private:
+    /** The callback for the viewer handler.
+     *
+     * @brief callback_viewer
+     * @param wsi Opaque websocket instance pointer
+     * @param reason The reason for the call
+     * @param user Pointer to per-session user data allocated by library
+     * @param in Pointer used for some callback reasons
+     * @param len Length set for some callback reasons
+     * @return is_success Boolean to see if the callback has success.
+     */
+    static int callback_http_dummy(struct lws *wsi, enum lws_callback_reasons reason,
+                                void *user, void *in, size_t len);
 
+    /** A dummy callback because the libwebsocket doesn't start if the http is not handled.
+     *
+     * @brief callback_viewer
+     * @param wsi Opaque websocket instance pointer
+     * @param reason The reason for the call
+     * @param user Pointer to per-session user data allocated by library
+     * @param in Pointer used for some callback reasons
+     * @param len Length set for some callback reasons
+     * @return 0 Boolean to say the callback has success.
+     */
+    static int callback_viewer(struct lws *wsi, enum lws_callback_reasons reason,
+                            void *user, void *in, size_t len);
+
+    /**
+     * @brief The protocols structure.
+     *
+     * Use for register all protocols.
+     * The tab is set to three because we have only three protocols registred.
+     */
+    struct lws_protocols protocols_[3];
+    /**
+     * @brief The context structure
+     * The struct lws_context represents the server.
+     */
+    struct lws_context * context_;
 public:
+   /**
+   * @brief Constructor.
+   *
+   * Create context, protocols and websocket.
+   */
   ViewerCommunication();
+  /**
+   * @brief runTask
+   *
+   * Use here to launch the callback function if there is a upcoming connection.
+   *
+   * @return success Boolean to see if the function has success.
+   */
   bool runTask() override;
+  /**
+   * @brief Destructor
+   *
+   * Destroy the context.
+   */
+  ~ViewerCommunication();
 };
 }
