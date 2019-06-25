3623e78cc3e8d672b9c45e80b4931f6b74c61fac
romainpc <romainpc.lechat@laposte.net>
2019-06-25 10:39:53 +0200

    Fix avoidance problem where 2 bots block and drift

--- src/ai/robot_behavior/navigation_with_obstacle_avoidance.h
+++ src/ai/robot_behavior/navigation_with_obstacle_avoidance.h
@@ -36,6 +36,7 @@ class NavigationWithObstacleAvoidance : public ConsignFollower
 private:
   bool avoided_;
   const double AVOID_MOMENT = 0.35;  // time in second before collision, when robot begin avoidance.
+  const double INFINITE_DODGING_PREVENTION = 1.0;
 
   bool ignore_the_ball_;
   bool ignore_robot_[2 * ai::Config::NB_OF_ROBOTS_BY_TEAM];  // 2 *  because there is 2 teams.
