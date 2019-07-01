398dea7e2ea7f8fd6e3111b8a24236a16a721554
romainpc <romainpc.lechat@laposte.net>
2019-07-01 15:19:26 +0200

    Recover obstructor

--- src/ai/manager/manual.cpp
+++ src/ai/manager/manual.cpp
@@ -33,6 +33,7 @@
 #include <robot_behavior/keeper/keeper.h>
 #include <robot_behavior/defender/defensive_wall.h>
 #include <robot_behavior/defender/kick_wall.h>
+#include <robot_behavior/obstructor.h>
 
 #include <strategy/keeper/keeper_strat.h>
 
@@ -138,6 +139,14 @@ Manual::Manual(std::string name) : Manager(name)
                                     },
                                     false  // we don't want to define a goal here !
                                     )));
+  registerStrategy("Obstructor", std::shared_ptr<strategy::Strategy>(new strategy::FromRobotBehavior(
+                                     [&](double time, double dt) {
+                                       robot_behavior::Obstructor* obstructor = new robot_behavior::Obstructor();
+                                       obstructor->declareRobotToObstruct(3);
+                                       return std::shared_ptr<robot_behavior::RobotBehavior>(obstructor);
+                                     },
+                                     false  // we don't want to define a goal here !
+                                     )));
   registerStrategy("Keeper Strat (need goalie)", std::shared_ptr<strategy::Strategy>(new strategy::KeeperStrat()));
 }
 
