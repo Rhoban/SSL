9eb2e7f561e2f67a10a2658f6375e20242e2825f
romainpc <romainpc.lechat@laposte.net>
2019-07-03 09:26:35 +0200

    Fix small warning

--- src/ai/strategy/offensive.cpp
+++ src/ai/strategy/offensive.cpp
@@ -123,13 +123,14 @@ bool Offensive::getStartingPositionForGoalie(rhoban_geometry::Point& linear_posi
 rhoban_ssl::annotations::Annotations Offensive::getAnnotations() const
 {
   rhoban_ssl::annotations::Annotations annotations;
-
-  for (auto it = this->getPlayerIds().begin(); it != this->getPlayerIds().end(); it++)
-  {
-    const rhoban_geometry::Point& robot_position = getRobot(*it).getMovement().linearPosition(time());
-    // annotations.addText("Behaviour: " + this->name, robot_position.getX() + 0.15, robot_position.getY(), "white");
-    // annotations.addText("Strategy: " + this->name, robot_position.getX() + 0.15, robot_position.getY() + 0.30, "white");
-  }
+  /*
+    for (auto it = this->getPlayerIds().begin(); it != this->getPlayerIds().end(); it++)
+    {
+      const rhoban_geometry::Point& robot_position = getRobot(*it).getMovement().linearPosition(time());
+      // annotations.addText("Behaviour: " + this->name, robot_position.getX() + 0.15, robot_position.getY(), "white");
+      // annotations.addText("Strategy: " + this->name, robot_position.getX() + 0.15, robot_position.getY() + 0.30,
+    "white");
+    }*/
   return annotations;
 }
 
