20c8ad6fc48e1f114500febb41fb3bcb127ab059
Victor Mignot <victor.mgnt@protonmail.com>
2019-07-03 05:38:19 +0200

    recover strategy defensive

--- src/ai/manager/manual.cpp
+++ src/ai/manager/manual.cpp
@@ -47,6 +47,7 @@
 #include <robot_behavior/defender/obstructor.h>
 #include <strategy/keeper/keeper_strat.h>
 
+#include <strategy/defensive.h>
 #include <strategy/wall.h>
 #include <strategy/wall_2.h>
 #include <robot_behavior/tests/test_infra.h>
@@ -203,7 +204,7 @@ Manual::Manual(std::string name) : Manager(name)
                                     },
                                     false  // we don't want to define a goal here !
                                     )));
-
+  registerStrategy("Defensive", std::shared_ptr<strategy::Strategy>(new strategy::Defensive()));
   registerStrategy("Wall1", std::shared_ptr<strategy::Strategy>(new strategy::Wall()));
   registerStrategy("Wall2", std::shared_ptr<strategy::Strategy>(new strategy::Wall_2()));
   registerStrategy("Obstructor", std::shared_ptr<strategy::Strategy>(new strategy::FromRobotBehavior(
